031fb0ed6a0005e285338874e2a3158f66970130
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-06-12 10:40:22 +0700

    tambah logika tampilan element dalam display

--- Queues.cpp
+++ Queues.cpp
@@ -69,10 +69,19 @@ public:
     void display(){
         int FRONT_position = FRONT;
         int REAR_position = REAR;
-        // cek apakah antrian kosong 
+        // cek apakah antrian kosong []
         if (FRONT == 1){
             cout << "Queue is empty\n";
             return;
+        }
+        cout << "nElements in the queue are ...\n";
+        //jika FRONT_position <= REAR_position iterasi dan FRONT
+        if (FRONT_position <= REAR_position){
+            while(FRONT_position <= REAR_position){
+                cout << queue_array[FRONT_position]<< "  ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
     }
 };
