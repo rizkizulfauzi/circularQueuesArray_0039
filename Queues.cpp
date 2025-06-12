0f0bd95c215aea85222fab3540e0f000da23dd1d
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-06-12 10:23:20 +0700

    menyelesaikan logika enqueue untuk circular enqueue

--- Queues.cpp
+++ Queues.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
 using namespace std;
 
-class Queues{
+class Queues
+{
     int FRONT, REAR, max = 5;
     int queue_array[5];
 
-    public:
-    Queues(){
+public:
+    Queues()
+    {
         FRONT = -1;
-        REAR =-1;
+        REAR = -1;
     }
-    void insert(){
+    void insert()
+    {
         int num;
-        cout <<"Enter a number : ";
+        cout << "Enter a number : ";
         cin >> num;
         cout << endl;
 
-        //cek antrian penuh
-        if ((FRONT == 0&& REAR == max -1)  || (FRONT == REAR +1)) {
+        // cek antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
             cout << "\nQueue overFlow\n";
             return;
         }
 
-        //cek antrian kosong
-        if (FRONT == -1){
+        // cek antrian kosong
+        if (FRONT == -1)
+        {
             FRONT = 0;
             REAR = 0;
         }
-        else {
-            //jika REAR  berada di posisi terakhir array, kembali ke awal array
-            if (REAR == max -1){
+        else
+        {
+            // jika REAR  berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
                 REAR = 0;
-            }
+
+            else
+                REAR = REAR + 1;
         }
     }
 };
